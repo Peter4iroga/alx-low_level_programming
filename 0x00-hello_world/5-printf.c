707a4f469ecf41573e0c04618d63a249df22f1b4
Peter4iroga <irogapeter4@gmail.com>
2022-03-11 20:42:58 -0800

    program that uses printf to output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
