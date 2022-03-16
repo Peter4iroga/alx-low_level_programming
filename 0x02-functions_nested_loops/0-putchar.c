d2095c1f9b7dd77c60d00a96c7aa091d38f7eb10
Peter4iroga <irogapeter4@gmail.com>
2022-03-16 00:11:16 -0700

    printing using loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,24 @@
-#include "main.h"
+# include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ *main - print out main car by char.
+ *Description: uses the main header file
+ *that contains the function _putchar
+ *Return: 0.
+ */
+
 int main(void)
 {
-         char str[] = "_putchar" 
-		 
-         int i;
-         i=0;
+char str[] = "_putchar";
+int i;
 
+i = 0;
 while (str[i])
-{ 	
-	_putchar(str[i]);
-        i++;
+{
+_putchar(str[i]);
+++i;
+}
 _putchar('\n');
 
-	return (0);
+return (0);
 }
